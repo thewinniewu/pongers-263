903e2a36202ee86321aa6ae2426669f6f6e0f5b5
Winnie Wu <weiwu@college.harvard.edu>
2015-11-28 00:25:47 -0500

    Generally working

--- app/src/main/java/com/www/pongers_263/MaliciousActivity.java
+++ app/src/main/java/com/www/pongers_263/MaliciousActivity.java
@@ -2,6 +2,11 @@ package com.www.pongers_263;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by winniewu on 11/27/15.
@@ -11,6 +16,19 @@ public class MaliciousActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_malicious);
+
+        EditText password = (EditText) this.findViewById(R.id.password);
+        password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    Toast.makeText(v.getContext(), "l0lz pwn3d",
+                            Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+        });
+
     }
 
 }
